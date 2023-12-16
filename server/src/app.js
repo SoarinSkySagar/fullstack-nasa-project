30a86c88db65d3681ac124716a1823dbce4dd0c9
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-12-16 23:16:16 +0530

    added morgan middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets/planets.router')
-const middlewares = require('./routes/middlewares/middlewares.controller')
 
 const app = express()
 
-app.use(middlewares.responseTime)
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'build')))
 app.use(planetsRouter)
