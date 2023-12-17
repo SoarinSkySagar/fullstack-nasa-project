708868bf017c9ad521f101151df5053cccde8e44
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-12-17 23:02:54 +0530

    updated client

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,9 +7,9 @@ const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customer: ['ZTM', 'NASA'],
-    upcoming: false,
+    upcoming: true,
     success: true
 }
 
