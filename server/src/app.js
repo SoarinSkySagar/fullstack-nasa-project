e10640c346eec75356bca10fe29115e00a13a316
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-12-18 12:31:36 +0530

    vercel adjustments

--- server/src/app.js
+++ server/src/app.js
@@ -13,11 +13,11 @@ app.use(cors({
 }))
 app.use(morgan('combined'))
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '..', 'build')))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/planets', planetsRouter)
 app.use('/launches', launchesRouter)
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
 module.exports = app
