d4a3e9767a6fe796d646c5dd7dcdcff536527f2a
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-12-22 23:17:25 +0530

    connected planets data to mongodb

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const parse = require('csv-parse');
 const fs = require('fs');
 const path = require('path');
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo')
 
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
@@ -17,23 +17,42 @@ function loadData() {
             comment: '#',
             columns: true,
         }))
-        .on('data', (data) => {
+        .on('data', async (data) => {
             if (isHabitablePlanet(data)) {
-            habitablePlanets.push(data);
+                savePlanet(data)
             }
         })
         .on('error', (err) => {
             console.log(err);
             rej(err)
         })
-        .on('end', () => {
-            console.log(`${habitablePlanets.length} habitable planets found!`);
+        .on('end', async () => {
+            const count = (await getPlanets()).length
+            console.log(`${count} habitable planets found!`);
             res()
         });
     })
 }
 
+async function getPlanets() {
+    return await planets.find({})
+}
+
+async function savePlanet(planet) {
+    try {
+        await planets.updateOne({
+            keplerName: planet.kepler_name
+        }, {
+            keplerName: planet.kepler_name
+        }, {
+            upsert: true
+        })
+    } catch(err) {
+        console.error(`Could not save planet ${err}`)
+    }    
+}
+
   module.exports = {
     loadData,
-    planets: habitablePlanets
+    getPlanets
   }
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,9 +1,9 @@
-const {planets} = require('../../models/planets.model')
+const {getPlanets} = require('../../models/planets.model')
 
-function getAllPlanets(req, res) {
-    return res.status(200).json(planets)
+async function getAllPlanets(req, res) {
+    return res.status(200).json(await getPlanets())
 }
 
 module.exports = {
     getAllPlanets,
-}
+}       
