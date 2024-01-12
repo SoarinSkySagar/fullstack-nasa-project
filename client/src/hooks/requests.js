e5d635250c92fa0e3d2308fd3ecb891dc6685036
SoarinSkySagar <sagarrana.pvt@gmail.com>
2024-01-12 21:27:54 +0530

    build pipeline env

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000/v1'
+const API_URL = process.env.REACT_APP_URL || 'http://localhost:8000/v1'
 
 async function httpGetPlanets() {
   const res = await fetch(`${API_URL}/planets`)
